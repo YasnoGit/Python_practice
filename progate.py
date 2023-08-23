bf0365859c383a6abf3b8573a0548bfd6b988843
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-08-23 10:26:39 +0000

    変数中断

--- progate.py
+++ progate.py
@@ -1,1 +1,21 @@
 print('Hello Python')
+print('Hello World')
+print(3)
+print(3 + 7)
+print(7 - 3)
+print('3 + 5')
+print(3 * 7)
+print(3 / 2)
+print(7 % 3)
+
+# 変数＝値
+name = 'john'
+print(name)
+number = 24
+print(number)
+
+# 変数計算
+apple_price = 200
+apple_count = 8
+total_praice = apple_price * apple_count
+print(total_praice)
