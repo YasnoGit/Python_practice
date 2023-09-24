390187e19a2423561000079320e17e90f0ff52b4
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-09-24 06:12:07 +0000

    Python繰り返し基礎

--- progate.py
+++ progate.py
@@ -34,3 +34,77 @@ age = 24
 print('私は' + str(age) + '歳です')
 count = '5'
 print(int(count) + 1)
+
+print('Hello Python')
+print('Hello World')
+print(3)
+print(3 + 7)
+print(7 - 3)
+print('3 + 5')
+print(3 * 7)
+print(3 / 2)
+print(7 % 3)
+
+# 変数＝値
+name = 'john'
+print(name)
+number = 24
+print(number)
+
+# 変数計算
+apple_price = 200
+apple_count = 8
+total_praice = apple_price * apple_count
+print(total_praice)
+
+# 変数更新
+money = 2000
+print(money)
+money = money + 5000
+print(money)
+
+myname = 'にゃんじゃわんこ'
+print('私は' + myname + 'です')
+
+# データ型
+age = 24
+print('私は' + str(age) + '歳です')
+count = '5'
+print(int(count) + 1)
+
+print('Hello Python')
+print('Hello World')
+print(3)
+print(3 + 7)
+print(7 - 3)
+print('3 + 5')
+print(3 * 7)
+print(3 / 2)
+print(7 % 3)
+
+# 変数＝値
+name = 'john'
+print(name)
+number = 24
+print(number)
+
+# 変数計算
+apple_price = 200
+apple_count = 8
+total_praice = apple_price * apple_count
+print(total_praice)
+
+# 変数更新
+money = 2000
+print(money)
+money = money + 5000
+print(money)
+
+myname = 'にゃんじゃわんこ'
+print('私は' + myname + 'です')
+
+# データ型
+age = 24
+print('私は' + str(age) + '歳です')
+count = '5'
+print(int(count) + 1)
